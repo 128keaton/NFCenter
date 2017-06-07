3adaff6d11a41aae20c215eb4554db51c304e5d8
Keaton Burleson <keaton.burleson@me.com>
2017-06-06 21:57:50 -0500

    Added a start/stop scanning option.

--- NFCenter/ViewController.swift
+++ NFCenter/ViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    var nfcSession: NFCNDEFReaderSession? = nil
+    
+    /* MARK: NFCNDEFReaderSessionDelegate */
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error.localizedDescription)
+        stopScanning()
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                print(record.identifier)
+                print(record.payload)
+                print(record.type)
+                print(record.typeNameFormat)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func beginScanning() {
+        if nfcSession == nil {
+            nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        }
+        nfcSession?.begin()
+    }
+    
+    func stopScanning() {
+        nfcSession?.invalidate()
+        nfcSession = nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        stopScanning()
         // Dispose of any resources that can be recreated.
     }
 

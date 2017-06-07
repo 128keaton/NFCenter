6a4207da5ba66b5ae10831297c9968bf271884c1
Keaton Burleson <keaton.burleson@me.com>
2017-06-06 23:21:51 -0500

    New line!

--- NFCenter/ViewController.swift
+++ NFCenter/ViewController.swift
@@ -11,10 +11,14 @@ import CoreNFC
 
 class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
     var nfcSession: NFCNDEFReaderSession? = nil
+    var newLog = true
+    @IBOutlet var logArea: UITextView?
+    
+    
     
     /* MARK: NFCNDEFReaderSessionDelegate */
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-        print(error.localizedDescription)
+        appendLog(newText: ["Error", error.localizedDescription])
         stopScanning()
     }
     
@@ -25,12 +29,16 @@ class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
                 print(record.payload)
                 print(record.type)
                 print(record.typeNameFormat)
+                appendLog(newText: [String(describing: record.identifier), String(describing: record.payload), String(describing: record.type), String(describing: record.typeNameFormat)])
             }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        logArea?.layer.cornerRadius = 10
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -46,6 +54,24 @@ class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
         nfcSession = nil
     }
     
+    func appendLog(newText: [String]) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .medium
+        
+        // Call main thread since the NFC thread is background!
+        DispatchQueue.main.sync {
+            if newLog == true{
+                logArea?.text = ""
+                newLog = false
+            }
+            logArea?.text.append(dateFormatter.string(from: Date()) + " ")
+            for string in newText {
+                logArea?.text.append(string + " ")
+            }
+            logArea?.text.append("\n")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         stopScanning()
